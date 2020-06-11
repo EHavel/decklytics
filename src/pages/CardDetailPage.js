a5accc700463c7ecae7cf6c9338541a06044099d
EHavel <eric.havel@gmail.com>
2020-06-11 11:02:25 -0300

    add ads

--- src/pages/CardDetailPage.js
+++ src/pages/CardDetailPage.js
@@ -75,6 +75,15 @@ const CardDetailPage = () => {
                     {card && (
                         <CardDetails card={card} key={card.cardCode} />
                     )}
+                    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                    <ins className="adsbygoogle"
+                        data-ad-layout="in-article"
+                        data-ad-format="fluid"
+                        data-ad-client="ca-pub-5888402531823458"
+                        data-ad-slot="5737051106"></ins>
+                    <script>
+                        (adsbygoogle = window.adsbygoogle || []).push({});
+                    </script>
                     {cardLevelUp && (
                         <>
                             <TitleDivider icon="levelup"
